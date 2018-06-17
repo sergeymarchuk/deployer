fbaa7429956ae63d35fd4e1c8b514c881e74c5b2
Yurii Svystun <svystun@gmail.com>
2018-06-17 23:03:44 +0300

    Rename title name.

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Roles-Permissions Manager',
+    'name' => 'Yurii Svystun Deployer Manager',
 
     /*
     |--------------------------------------------------------------------------
