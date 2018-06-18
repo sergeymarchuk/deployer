71bc96be27a9965c84c3e9e1c3eb11e1adbe9d74
Yurii Svystun <svystun@gmail.com>
2018-06-18 23:37:45 +0300

    Optimize structure.

--- app/Service/RemoteArtisan.php
+++ app/Service/RemoteArtisan.php
@@ -1,4 +1,4 @@
-<?php namespace App;
+<?php namespace App\Service;
 
 use Dotenv\Dotenv;
 use Illuminate\Support\Str;
@@ -12,20 +12,52 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 class RemoteArtisan
 {
     /**
-     * Implements an interface similar to Artisan::call.
+     * @var $path
+     */
+    protected $path;
+
+    /**
+     * @var $command
+     */
+    protected $command;
+
+    /**
+     * @var array $parameters
+     */
+    protected $parameters = [];
+
+    /**
+     * @var null $php
+     */
+    protected $php = null;
+
+    /**
+     * RemoteArtisan constructor.
      *
      * @param string $path The full path to the remote application's root
      * @param string $command The Artisan command to run, e.g. 'vendor:publish'
      * @param array $parameters The parameters to pass to the artisan command
      * @param string $php The path to the PHP executable
+     */
+    public function __construct(string $path, string $command, $parameters = [], $php = null)
+    {
+        $this->path = $path;
+        $this->command = $command;
+        $this->parameters = $parameters;
+        $this->php = $php;
+    }
+
+    /**
+     * Implements an interface similar to Artisan::call.
+     *
      * @return mixed
      * @throws ProcessFailedException
      */
-    public static function call($path, $command, $parameters = [], $php = null)
+    public function run()
     {
         // Prepare parameters for appending
         $options = '';
-        foreach ($parameters as $name => $value) {
+        foreach ($this->parameters as $name => $value) {
 
             if (is_int($name)) {
                 $options .= " $value";
@@ -36,22 +68,22 @@ class RemoteArtisan
         }
 
         // Swap out the full path to the current PHP executable binary
-        if (! $php) {
-            $php = PHP_BINDIR . '/php';
+        if (! $this->php) {
+            $this->php = PHP_BINDIR . '/php';
         }
 
         // Load .env for the target app
-        $env = new Dotenv($path);
+        $env = new Dotenv($this->path);
         $env->overload();
 
         // Append the artisan command to the path
-        $artisan = Str::finish($path, '/').'artisan';
+        $artisan = Str::finish($this->path, '/').'artisan';
 
         // Build up the final command
-        $command = "{$php} {$artisan} {$command}{$options}";
+        $command = "{$this->php} {$artisan} {$this->command}{$options}";
 
         // Run the command in its environment
-        $process = new Process($command, $path);
+        $process = new Process($command, $this->path);
         $process->run();
 
         // Restore original environment
