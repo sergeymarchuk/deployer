a81d5703412e56226fb16ece5d309f66f7e4b9bd
Yurii Svystun <svystun@gmail.com>
2018-07-15 17:37:12 +0300

    Adder back url and status code and text.

--- resources/views/errors/http-error.blade.php
+++ resources/views/errors/http-error.blade.php
@@ -40,7 +40,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">{{ $statusCode }} {{ $statusText }}</div>
+                <div><a href="{{ route('admin.home') }}">Go home</a></div>
             </div>
         </div>
     </body>
