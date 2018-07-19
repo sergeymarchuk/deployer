e4f6393fdaa063df5da70919378230dd8ae1ac9f
Yurii Svystun <svystun@gmail.com>
2018-07-20 00:29:44 +0300

    Added new permission deploy.

--- database/seeds/RoleSeed.php
+++ database/seeds/RoleSeed.php
@@ -13,7 +13,7 @@ class RoleSeed extends Seeder
     public function run()
     {
         $admin = Role::create(['name' => 'administrator']);
-        $admin->givePermissionTo('users_manage', 'projects_manage');
+        $admin->givePermissionTo('users_manage', 'projects_manage', 'deploy');
         $deployer = Role::create(['name' => 'deployer']);
         $deployer->givePermissionTo('deploy');
     }
