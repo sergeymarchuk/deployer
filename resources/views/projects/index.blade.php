2c3b781d48145602c348a16d01561ac704e23771
Yurii Svystun <svystun@gmail.com>
2018-07-20 00:37:22 +0300

    Fixed code style.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,16 +1,9 @@
 @extends('layouts.app')
 @section('content')
     <h3 class="page-title">Projects</h3>
-
-    <p>
-        <a href="{{ route('projects.create') }}" class="btn btn-success">Add new</a>
-    </p>
-
+    <p><a href="{{ route('projects.create') }}" class="btn btn-success">Add new</a></p>
     <div class="panel panel-default">
-        <div class="panel-heading">
-            List
-        </div>
-
+        <div class="panel-heading">List</div>
         <div class="panel-body">
             <table class="table table-bordered table-striped {{ count($projects) > 0 ? 'datatable' : '' }} dt-select">
                 <thead>
@@ -36,17 +29,16 @@
                                 <td>{{ $project->budget }}</td>
                                 <td>{{ $project->project_status->title or '' }}</td>
                                 <td>
-                                <a href="{{ route('documents.index',['project' => $project->id]) }}" class="btn btn-xs btn-default">Documents</a>
-                                <a href="{{ route('transactions.index',['project' => $project->id]) }}" class="btn btn-xs btn-default">Transactions</a>
-                                <a href="{{ route('notes.index',['project' => $project->id]) }}" class="btn btn-xs btn-default">Notes</a><br>
-                                <a href="{{ route('projects.show',[$project->id]) }}" class="btn btn-xs btn-primary">View</a>
-                                <a href="{{ route('projects.edit',[$project->id]) }}" class="btn btn-xs btn-info">Edit</a>{!! Form::open(array(
-                'style' => 'display: inline-block;',
-                'method' => 'DELETE',
-                'onsubmit' => "return confirm('".trans("Are you sure?")."');",
-                'route' => ['projects.destroy', $project->id])) !!}
-    {!! Form::submit('Delete', array('class' => 'btn btn-xs btn-danger')) !!}
-    {!! Form::close() !!}</td>
+                                    <a href="{{ route('projects.show',[$project->id]) }}" class="btn btn-xs btn-primary">View</a>
+                                    <a href="{{ route('projects.edit',[$project->id]) }}" class="btn btn-xs btn-info">Edit</a>
+                                    {!! Form::open(array(
+                                    'style' => 'display: inline-block;',
+                                    'method' => 'DELETE',
+                                    'onsubmit' => "return confirm('".trans("Are you sure?")."');",
+                                    'route' => ['projects.destroy', $project->id])) !!}
+                                    {!! Form::submit('Delete', array('class' => 'btn btn-xs btn-danger')) !!}
+                                    {!! Form::close() !!}
+                                </td>
                             </tr>
                         @endforeach
                     @else
