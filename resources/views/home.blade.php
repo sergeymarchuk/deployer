91b79ef767ba6549c8c16a6020df8e4bd9eaff65
Yurii Svystun <svystun@gmail.com>
2018-07-22 03:44:34 +0300

    Refactored home page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 @section('content')
-    <h3 class="page-title">Deployment for project: <a href="http://stage.cf15.pro" target="_blank">http://stage.CF15.pro</a></h3>
-
     @if(session('success'))
         <!-- If password successfully show message -->
         <div class="row">
@@ -10,65 +8,16 @@
             </div>
         </div>
     @else
-        {!! Form::open(['method' => 'POST', 'route' => ['auth.change_password']]) !!}
-        <!-- If no success message in flash session show change password form  -->
-        <p>
-            {!! Form::button(trans('global.app_deploy'), ['class' => 'btn btn-danger deploy-start']) !!}
-            &nbsp;&nbsp;&nbsp;&nbsp;{!! Form::button('Clear', ['class' => 'btn btn-primary deploy-clear']) !!}
-        </p>
+        <h2>Hello, {{ Auth::user()->name }}</h2>
         <div class="panel panel-default">
-            <div class="panel-heading">Deployment steps:</div>
+            <div class="panel-heading">Info</div>
             <div class="panel-body">
-
                 <div class="row">
-                    <div class="col-xs-12 form-group">
-                        {!! Form::label('gitPull', 'Step 1: Update codebase (command: git pull)', ['class' => 'control-label']) !!}
-                        {!! Form::textarea('gitPull', null, ['class' => 'form-control', 'placeholder' => '', 'rows' => 1]) !!}
-                        <p class="help-block"></p>
-                        @if($errors->has('current_password'))
-                            <p class="help-block">
-                                {{ $errors->first('current_password') }}
-                            </p>
-                        @endif
+                    <div class="col-xs-12">
+                        {!! Form::label('txt', 'Your last login: '. Auth::user()->updated_at, ['class' => 'control-label']) !!}
                     </div>
                 </div>
-
-                <div class="row">
-                    <div class="col-xs-12 form-group">
-                        {!! Form::label('composerInstall', 'Step 2: Update packages (command: composer install)', ['class' => 'control-label']) !!}
-                        {!! Form::textarea('composerInstall', null, ['class' => 'form-control', 'placeholder' => '', 'rows' => 1]) !!}
-                        <p class="help-block"></p>
-                        @if($errors->has('new_password'))
-                            <p class="help-block">
-                                {{ $errors->first('new_password') }}
-                            </p>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-xs-12 form-group">
-                        {!! Form::label('artisanMigrate', 'Step 3: Update database structure (command: php artisan migrate)', ['class' => 'control-label']) !!}
-                        {!! Form::textarea('artisanMigrate', null, ['class' => 'form-control', 'placeholder' => '', 'rows' => 1]) !!}
-                        <p class="help-block"></p>
-                        @if($errors->has('new_password_confirmation'))
-                            <p class="help-block">
-                                {{ $errors->first('new_password_confirmation') }}
-                            </p>
-                        @endif
-                    </div>
-                </div>
-
             </div>
         </div>
-
-        <p>
-            {!! Form::button(trans('global.app_deploy'), ['class' => 'btn btn-danger deploy-start']) !!}
-            &nbsp;&nbsp;&nbsp;&nbsp;{!! Form::button('Clear', ['class' => 'btn btn-primary deploy-clear']) !!}
-        </p>
-        {!! Form::close() !!}
     @endif
-@stop
-@section('javascript')
-    <script src="{{ url('js/deploy.js') }}"></script>
 @stop
