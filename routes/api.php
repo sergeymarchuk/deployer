e8095fa9d730053ed543d79d633e40219264c927
Sergey Marchuk <sm@marich.pro>
2018-09-23 10:09:11 +0300

    correct route

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,5 @@
 
 // route for github webhook api
 Route::group(['prefix' => '/v1', 'namespace' => 'API\V1', 'as' => 'api.'], function () {
-    Route::match(['get', 'post'], '/{$slug}', 'WebhookController@runDeploy');
+    Route::post('webhook/{slug}', 'WebhookController@runDeploy');
 });
