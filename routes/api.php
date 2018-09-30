5b5e9018bed857ad4a69884fcebf86a92da3befe
Yurii Svystun <svystun@gmail.com>
2018-10-01 00:51:22 +0300

    Fixed api route.

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
 // route for github webhook api
-//Route::post('test', 'WebhookController@runDeploy');
-Route::group(['prefix' => 'v2', 'as' => 'api.'], function () {
+Route::group(['prefix' => 'v1', 'as' => 'api.'], function() {
+    Route::post('{slug}', 'WebhookController@runDeploy');
 });
-
-Route::post('/v2/{slug}', 'WebhookController@runDeploy');
 
