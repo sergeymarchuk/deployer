755fdfab5165e2eb28cfd70b7da0deb52fc79eb7
Sergey Marchuk <sm@marich.pro>
2018-10-13 16:07:18 +0300

    refactor DeploymentService

--- app/Services/DeploymentService.php
+++ app/Services/DeploymentService.php
@@ -1,6 +1,8 @@
 <?php namespace App\Services;
 
 use App\Models\Project;
+use Dotenv\Dotenv;
+use Illuminate\Support\Str;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
@@ -26,16 +28,20 @@ class DeploymentService
      */
     public function runAction(Project $project, string $action, string $response = 'jsonp')
     {
-        //TODO: Check if path exists and artisan file
-
-        $process = ($action == 'artisan-migrate') ?
-            new ArtisanService($project->path, self::COMMANDS[$action], ['--force' => true]) :
-            new Process(self::COMMANDS[$action], $project->path);
+        if (!is_dir($project->path)) {
+            return false; //maybe throw Exception
+        }
 
         try {
-            $text = ($process instanceof Process) ?
-                $process->mustRun()->getOutput() :
-                $process->run();
+
+            if ($action == 'artisan migrate') {
+                $options = $this->getOptions(['--force' => true]);
+                $text = $this->run($project->path, self::COMMANDS[$action], $options);
+            } else {
+                $process = new Process(self::COMMANDS[$action], $project->path);
+                $text = $process->mustRun()->getOutput();
+            }
+
             $status = self::STATUS_OK;
         } catch (ProcessFailedException $exception) {
             $text = $exception->getMessage();
@@ -62,5 +68,55 @@ class DeploymentService
             'next' => request('next', ''),
             'prev' => request('prev', '')
         ]);
+    }
+
+    protected function run(string $path, string $command, $options, $php = null) {
+
+        // Swap out the full path to the current PHP executable binary
+        if (! $php) {
+            $php = PHP_BINDIR . '/php';
+        }
+
+        // Load .env for the target app
+        $env = new Dotenv($path);
+        $env->overload();
+
+        // Run the command in its environment
+        $process = new Process($this->getCommand($path, $command, $php, $options ), $path);
+        $process->run();
+
+        // Restore original environment
+        $env = new Dotenv(base_path());
+        $env->overload();
+
+        // If there was no response
+        if (! $process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        return $process->getOutput();
+    }
+
+    private function getCommand($path, $command, $php, $options) {
+        // Append the artisan command to the path
+        $artisan = Str::finish($path, '/').'artisan';
+
+        // Build up the final command
+        return "{$php} {$artisan} {$command}{$options}";
+    }
+
+    private function getOptions($parameters = []) {
+        $options = '';
+        foreach ($parameters as $name => $value) {
+
+            if (is_int($name)) {
+                $options .= " $value";
+            } else {
+                $value = $value === true ? '' : '="'.$value.'"';
+                $options .= " {$name}{$value}";
+            }
+        }
+
+        return $options;
     }
 }
